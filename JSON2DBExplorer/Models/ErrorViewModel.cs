af9b53c2b0f11f7f79d481cdb1512ef07820eeb2
Stanislav <rotarstanislav4@gmail.com>
2024-01-18 15:44:31 +0200

    Init

---
+++ JSON2DBExplorer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JSON2DBExplorer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
